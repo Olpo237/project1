c0bcc29ecc5e9437f95574843068405b81ce6141
Olpo237 <97330964+Olpo237@users.noreply.github.com>
2023-05-15 12:35:55 +0200

    first portfolio

---
+++ portfolio/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react'; 
+import ReactDOM from 'react-dom/client'; 
+import App from './App';
+
+ReactDOM.render(<APP/> , document.getElementById('root')); //THIS LINE INDICATE THAT 
+// ALL THE CONTENT OF THIS FILE SHOULD BE RENDERED IN THE ROOT ID OF THE HTML FILE 
